76b5220c0d2fe579a73c5528f3ed446157ef5fdd
DanielMarella <marella.daniel96@gmail.com>
2023-05-12 16:15:23 +0200

    add age and km request, add some html for better visualizzation on page

---
+++ js/myscript.js
@@ -0,0 +1,12 @@
+
+
+
+const PassengerAge = prompt parseInt('Quanti anni hai?');
+
+document.getElementById('YourAge').innerHTML = PassengerAge;
+
+const KmToDo = prompt('Quanti km vuoi percorrere?');
+
+document.getElementById('YourKm').innerHTML = KmToDo;
+
+
