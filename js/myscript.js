4cc16c166256d5adb38f6b030e351210e81e536a
DanielMarella <marella.daniel96@gmail.com>
2023-05-12 17:05:57 +0200

    add discount for minor and major age

--- js/myscript.js
+++ js/myscript.js
@@ -1,12 +1,28 @@
 
 
 
-const PassengerAge = prompt parseInt('Quanti anni hai?');
-
+const PassengerAge = prompt ('Quanti anni hai?');
 document.getElementById('YourAge').innerHTML = PassengerAge;
 
 const KmToDo = prompt('Quanti km vuoi percorrere?');
-
 document.getElementById('YourKm').innerHTML = KmToDo;
+
+const underageDiscount = ('20%');
+
+const FullTicket = parseInt(0.21 * KmToDo);
+document.getElementById('fullPrice').innerHTML = FullTicket;
+
+const DiscountTiket = FullTicket ;
+if (PassengerAge < 18){
+
+    const DiscountTiket = (parseInt(underageDiscount / 100) * FullTicket);
+    document.getElementById('minorDiscount').innerHTML = DiscountTiket;
+}  
+
+else if (PassengerAge > 65){
+    const DiscountTiket = (65 / 100) * FullTicket;
+    document.getElementById('majorDiscount').innerHTML = DiscountTiket;
+}
+
 
 
