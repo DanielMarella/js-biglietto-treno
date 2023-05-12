7f2e13cf36ac1e4b06d7ab0b1361555fa9e9eb8d
DanielMarella <marella.daniel96@gmail.com>
2023-05-12 17:11:20 +0200

    fix js for make html display 2 decimal number post the comma, add the € symbol after the result

--- js/myscript.js
+++ js/myscript.js
@@ -8,19 +8,17 @@ const KmToDo = prompt('Quanti km vuoi percorrere?');
 document.getElementById('YourKm').innerHTML = KmToDo;
 
 const FullTicket = parseInt(0.21 * KmToDo);
-document.getElementById('fullPrice').innerHTML = FullTicket;
+document.getElementById('fullPrice').innerHTML = FullTicket.toFixed(2) + '€';
 
 const DiscountTiket = FullTicket ;
 if (PassengerAge < 18){
 
     const DiscountTiket = (20 / 100) * FullTicket;
-    document.getElementById('minorDiscount').innerHTML = DiscountTiket;
+    document.getElementById('minorDiscount').innerHTML = DiscountTiket.toFixed(2) + '€';
 }  
 
 else if (PassengerAge > 65){
     const DiscountTiket = (40 / 100) * FullTicket;
-    document.getElementById('majorDiscount').innerHTML = DiscountTiket;
+    document.getElementById('majorDiscount').innerHTML = DiscountTiket.toFixed(2) + '€';
 }
-
-
 
